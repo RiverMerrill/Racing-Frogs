cfa8d895ef1be29ba63f2eec459a1f8bcb0128fa
River Merrill <rivermerrill.me@gmail.com>
2015-09-30 21:01:32 -0600

    Added functionality

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var app = angular.module('racingFrogs', []);
 app.controller('MainController', MainController);
 //No need to change anything above this line.
-
+var namesList = [
+    'Jo', 'Pinkie', 'Ezekiel', 'Cherri', 'Jacinto', 'Taisha', 'Vivien', 'Bradford', 'Floretta', 'Jennell', 'Josette', 'Alma', 'Zandra', 'Juliette', 'Jeri', 'Chery', 'Justina', 'Lelia', 'Melani', 'Zula'
+]
 function MainController() {
     var vm = this; //instead of using this when refering to the controller, let's use vm. It will make things easier.
     vm.joe = new Guy('Joe', 100);
     vm.bob = new Guy('Bob', 100);
     vm.bank = 200;
+
     function Guy(name, startingCash) {
         this.name = name;
         this.cash = startingCash;
@@ -48,18 +51,112 @@ function MainController() {
     vm.receiveMoneyFromJoe = function () {
         vm.bank += vm.joe.giveCash(5);
     }
-    
-    function Frog(name, posX){
+
+    function Frog(name, posX, bet) {
         this.name = name;
         this.posX = posX;
+        this.bet = bet;
     }
-    vm.frogs = [new Frog('Joe', 144), new Frog('Sam', 144), new Frog('Bill', 144)];
-    vm.startRace = function(){
-    vm.frogs.forEach(function(frog){
-        frog.posX += 12;
-        if(frog.posX >= 876){
-            alert(frog.name + ' has won!')
+    vm.frogs = [];
+    vm.addFrog = function (name) {
+        name = $('.frogAdderInput')
+        if (name.val() === '') {
+            var newFrog = new Frog(namesList[Math.floor((Math.random() * namesList.length) + 1)], 144, 0);
+            vm.frogs.push(newFrog);
+            name.val('');
         }
-    })
+        else {
+            var newFrog = new Frog(name.val(), 144, 0);
+            vm.frogs.push(newFrog);
+            name.val('');
+        }
+    }
+    vm.startRace = function () {
+        var frogsArr = [];
+        if (vm.frogs.length < 2) {
+            alert('a race has to have at least 2 contestants before it can start or accept bets');
+        } else {
+            vm.frogs.forEach(function (frog) {
+                while(frog.posX < 700){
+                frog.posX += Math.floor((Math.random() * 100) + 1);
+                if (frog.posX > 144) {
+                    $('.frogAdderInput, frogAdderButton').attr('disabled', true);
+                }
+                if (frog.posX >= 700) {
+                    frogsArr.push(frog);
+                    break;
+                }
+               }
+            })
+            if (frogsArr.length > 1) {
+                alert('Looks like it\'s a tie between ' + frogsArr[0].name + " and " + frogsArr[1].name + '!');
+                vm.frogs.forEach(function(frog){
+                frog.posX = 144;
+                });
+                return;
+            } else {
+                alert(frogsArr[0].name + ' has won!');
+                if (frogsArr[0].bet > 0) {
+                    vm.cashOnHand.amount += parseInt(frogsArr[0].bet);
+                }
+            }
+            vm.frogs.forEach(function(frog){
+                frog.posX = 144;
+            })
+        }
+    }
+    vm.newRace = function () {
+        vm.frogs = [];
+        $('.frogAdderInput').val('');
+        $('.frogAdderInput').attr('disabled', false);
+    }
+    vm.cashOnHand = {
+        amount: 10
+    }
+    vm.bet = function (index) {
+        vm.frogs[index].bet = vm.subtract;
+        if (vm.frogs[index].bet < 1) {
+            $('<p></p>').text('Nice try, buddy.').addClass('timeout').appendTo('.panel-title');
+            setTimeout(function () {
+                $('.timeout').fadeOut('slow');
+            }, 3000)
+        } else {
+            $('<p></p>').text('You bet $' + vm.frogs[index].bet + ' on ' + vm.frogs[index].name).addClass('timeout').appendTo('.panel-title');
+            setTimeout(function () {
+                $('.timeout').fadeOut('slow');
+            }, 3000)
+            vm.cashOnHand.amount -= vm.subtract;
+            vm.subtract = '';
+        }
     }
 }
+
+
+
+
+
+
+
+/*
+app.service('BettingService', BettingService);
+
+function BettingService(){
+    var _races = {};
+    var _raceId = 0;
+    this.registerRace = function(){
+        var newRace = new Race();
+        return newRace.id;
+    }
+    this.getRace = function(raceId) {
+        return Race.raceId;
+    }
+    var Race = function(){
+        this.id = _raceId;
+        this.tickets = 1300;
+        this.contestants = [];
+        this.open = true;
+        this.bets = {};
+        _races[this.id] = this;
+        _raceId++;
+    }
+} */
